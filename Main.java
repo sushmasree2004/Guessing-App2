461c4dee745254526d360924179b54bd9b88c65b
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-23 16:28:25 +0530

    storage service added to Main function

--- Main.java
+++ Main.java
@@ -1,40 +1,54 @@
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Main 
 {
-    public static void main(String args[]) throws InvalidInputException 
-    {
+    public static void main(String args[]) 
+	{
         System.out.println("Welcome To GUESSING APP ");
-
+		
         GameConfig game = new GameConfig();
         game.showRules();
 
         Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Enter your name: ");
+        String player = sc.nextLine();
+
         int attempts = 0;
+        boolean win = false;
 
         while (attempts < game.getMax_Attempts()) 
-        {
+		{
             System.out.print("Enter Your Guess : ");
+            try 
+			{
+                int guess = ValidationService.validateInput(sc.nextLine());
+                attempts++;
 
-            int guess = ValidationService.validateInput(sc.nextLine());
-            attempts++;
+                String result = GuessValidator.validateGuess(guess, game.getTargetNumber());
+                System.out.println(result);
 
-            String result = GuessValidator.validateGuess(guess, game.getTargetNumber());
-            System.out.println(result);
-
-            if (" CORRECT ".equals(result)) 
-            {
-                break;
+                if (" CORRECT ".equals(result)) 
+				{
+                    win = true;
+                    break;
+                }
+				else 
+				{
+                    if (attempts <= game.getMax_Hints()) 
+					{
+                        System.out.println(HintService.generateHint(game.getTargetNumber(), attempts));
+                    }
+                }
             }
 
-            if (!" CORRECT ".equals(result)) 
-            {
-                if (attempts <= game.getMax_Hints()) 
-                {
-                    System.out.println(HintService.generateHint(game.getTargetNumber(), attempts));
-                }
+			catch (InvalidInputException e) 
+			{
+                System.out.println(e.getMessage());
             }
         }
 
+        StorageService.saveResult(player, attempts, win);
     }
 }
