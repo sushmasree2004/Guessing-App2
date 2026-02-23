0d1e9ca6e1b345173424a8703defb48c3395fc84
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-23 16:46:51 +0530

    Game Restart feature added to Main

--- Main.java
+++ Main.java
@@ -1,54 +1,69 @@
-
 import java.util.*;
+import java.lang.*;
+
+
+import java.io.*;
+
 
 public class Main 
 {
     public static void main(String args[]) 
 	{
-        System.out.println("Welcome To GUESSING APP ");
-		
-        GameConfig game = new GameConfig();
-        game.showRules();
-
         Scanner sc = new Scanner(System.in);
-		
-        System.out.print("Enter your name: ");
-        String player = sc.nextLine();
 
-        int attempts = 0;
-        boolean win = false;
+        boolean playAgain = true;
 
-        while (attempts < game.getMax_Attempts()) 
+        while (playAgain) 
 		{
-            System.out.print("Enter Your Guess : ");
-            try 
+            System.out.println("Welcome To GUESSING APP ");
+			
+            GameConfig game = new GameConfig();
+            game.showRules();
+
+            System.out.print("Enter your name: ");
+            String player = sc.nextLine();
+
+            int attempts = 0;
+            boolean win = false;
+
+            while (attempts < game.getMax_Attempts()) 
 			{
-                int guess = ValidationService.validateInput(sc.nextLine());
-                attempts++;
-
-                String result = GuessValidator.validateGuess(guess, game.getTargetNumber());
-                System.out.println(result);
-
-                if (" CORRECT ".equals(result)) 
+                System.out.print("Enter Your Guess : ");
+                try 
 				{
-                    win = true;
-                    break;
-                }
-				else 
-				{
-                    if (attempts <= game.getMax_Hints()) 
+                    int guess = ValidationService.validateInput(sc.nextLine());
+                    attempts++;
+
+                    String result = GuessValidator.validateGuess(guess, game.getTargetNumber());
+                    System.out.println(result);
+
+                    if (" CORRECT ".equals(result)) 
 					{
-                        System.out.println(HintService.generateHint(game.getTargetNumber(), attempts));
+                        win = true;
+                        break;
+                    } 
+					else 
+					{
+                        if (attempts <= game.getMax_Hints()) 
+						{
+                            System.out.println(HintService.generateHint(game.getTargetNumber(), attempts));
+                        }
                     }
+                } 
+				
+				catch (InvalidInputException e) 
+				{
+                    System.out.println(e.getMessage());
                 }
             }
 
-			catch (InvalidInputException e) 
-			{
-                System.out.println(e.getMessage());
-            }
+            StorageService.saveResult(player, attempts, win);
+            System.out.println("Game result saved to game_results.txt");
+
+            
+            playAgain = GameController.restartGame(sc);
         }
 
-        StorageService.saveResult(player, attempts, win);
+        System.out.println("Thanks for playing");
     }
 }
